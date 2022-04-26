b9fd5a8c62f7ccd339bbfe92be8eefbc3a992ba8
Mirmuxsin <millykhamroev@gmail.com>
2022-04-26 23:05:56 +0500

    commit

--- src/Request.php
+++ src/Request.php
@@ -32,7 +32,7 @@ class Request
 
             $client = $guzzle->request('POST', 'https://api.telegram.org/bot' . $this->token . '/' . $method, [
                 'query' => $array,
-                'http_errors' => false
+                'http_errors' => false,
             ]);
 
             return $client->getBody();
