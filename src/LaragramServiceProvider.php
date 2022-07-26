15d8339b776a1c7f27890fa432cac23aa7625772
Muhammaddiyor Tohirov <mtohirov60@gmail.com>
2022-07-27 00:48:23 +0500

    add(routes, config), update(laragram artisan command)

--- src/LaragramServiceProvider.php
+++ src/LaragramServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace Milly\Laragram;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
-use Milly\Laragram\app\Console\Commands\longpolling;
 
 class LaragramServiceProvider extends ServiceProvider
 {
+    protected \Closure $laragramRoutes;
+
     /**
      * Bootstrap any package services.
      *
@@ -14,23 +16,68 @@ class LaragramServiceProvider extends ServiceProvider
      */
     public function boot (): void
     {
-        //$this->loadRoutesFrom(__DIR__.'/test/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations/');
 
-        //$this->publishes([
-        //    __DIR__.'/config.php' => config_path('laragram.php'),
-        //]);
+        $this->publishes([
+            __DIR__ . '/config/laragram.php' => config_path('laragram.php'),
+        ], 'laragram-config');
+
+        $this->publishes([
+            __DIR__ . '/routes/laragram.php' => base_path('laragram.php')
+        ], 'laragram-routes');
+
+        $this->loadRoutes();
 
         if ($this->app->runningInConsole()){
-            $this->commands([longpolling::class, \Milly\Laragram\app\Console\Commands\laragram::class]);
+            $this->commands([
+                \Milly\Laragram\app\Console\Commands\longpolling::class,
+                \Milly\Laragram\app\Console\Commands\laragram::class
+            ]);
         }
-
-
     }
 
     public function register()
     {
-
+        //
     }
 
+    /**
+     * Register laragram routes.
+     *
+     * @return void
+     */
+    public function loadRoutes()
+    {
+        $this->routes(function () {
+            Route::get('_laragram', function () {
+                require base_path('routes/laragram.php');
+            })->middleware('api')->name('_laragram');
+        });
+
+        if ($this->app->routesAreCached()) {
+            $this->app->booted(function () {
+                require $this->app->getCachedRoutesPath();
+            });
+        } else {
+            $this->app->call($this->laragramRoutes);
+
+            $this->app->booted(function () {
+                $this->app['router']->getRoutes()->refreshNameLookups();
+                $this->app['router']->getRoutes()->refreshActionLookups();
+            });
+        }
+    }
+
+    /**
+     * Register laragram routes.
+     *
+     * @param \Closure $callback
+     * @return $this
+     */
+    public function routes(\Closure $callback)
+    {
+        $this->laragramRoutes = $callback;
+
+        return $this;
+    }
 }
--- src/app/Console/Commands/longpolling.php
+++ src/app/Console/Commands/longpolling.php
@@ -26,29 +26,81 @@ class longpolling extends Command
      *
      * @return void
      */
-
     public function handle()
     {
         $this->info('Telegram bot starting on long-polling mode...');
-        $token = env('TELEGRAM_BOT_TOKEN');
+        $token = config('laragram.token');
 
-        $process = new Process(['npx', 'lt', '--port', '8000']);
-        $process->start();
-        $url = null;
-        while ($process->isRunning()) {
-            sleep(0.5);
-            $output = $process->getOutput();
-            if (preg_match('#https://[\w\-.]+#', $output, $matches)) {
-                $url = $matches[0] . route('/', absolute: false);
-                break;
-            }
+        if (empty($token)) {
+            $this->error('Telegram bot token is required');
+            exit();
         }
-        if ($url === null) {
-            $this->error('Localtunnel didn\'t return url');
-            return 1;
-        }
-        $this->info('Webhook adress: ' . $url);
-        file_get_contents("https://api.telegram.org/bot".$token."/setWebhook?url=$url");
+
+        if (!$this->existTunnel()) return;
+        $url = $this->serve();
+
+        $this->info('Webhook address: ' . $url);
+        file_get_contents("https://api.telegram.org/bot" . $token . "/setWebhook?url=" . $url);
+        $this->info('Webhook was successfully set');
         $this->call('serve');
+    }
+
+    /**
+     * Check if exist localtunnel server
+     *
+     * @return boolean
+     */
+    public function existTunnel()
+    {
+        $expression = '/([0-9]\.?)+$/'; // Regular expression. https://regex101.com/r/r8a7eB/1
+
+        $process = new Process(['lt', '--version']);
+        $process->start();
+
+        while ($process->isRunning()) {
+            sleep(1);
+            $output = $process->getOutput();
+
+            if (!empty($output)) break;
+        }
+
+        if (!preg_match($expression, $output)) {
+            $this->warn("\nLocaltunnel is not found
+                \rPlease run this command in terminal:");
+            $this->info("npm install --global localtunnel");
+
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Start localtunnel server
+     *
+     * @var array $url
+     * @return string
+     */
+    public function serve()
+    {
+        $expression = "/(https?:\/{2}[\w+-]+\.\w+\.\w+)$/"; // Regular expression. https://regex101.com/r/2c6sZC/1
+
+        $process = new Process(['lt', '--port', '8000']);
+        $process->start();
+
+        while ($process->isRunning()) {
+            sleep(1);
+            $output = $process->getOutput();
+
+            if (!empty($output)) break;
+        }
+
+        if (!preg_match($expression, $output, $url)) {
+            $this->error("\nLocaltunnel didn't return url");
+
+            exit();
+        }
+
+        return $url[0] . route('_laragram', absolute: false); // return webhook payload url
     }
 }
