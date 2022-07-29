dbf544ef00ce40f553098a6e5c4df69c873ec482
Mirmuxsin Khamroev <58307677+Mirmuxsin@users.noreply.github.com>
2022-07-29 10:53:24 +0500

    Deleted routes file
    Deleted routes file

--- src/routes/laragram.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Milly\Laragram\FSM\FSM;
-use App\Http\Controllers\HomeController;
-
-FSM::route('', [HomeController::class, 'home'], [
-    (new \Milly\Laragram\Types\Update())->message
-]);
